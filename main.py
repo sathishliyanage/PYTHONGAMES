f4079333a4e0239e3dee6fd8c510801e4c753d44
Sathish Liyanage <sathishliyanage@gmail.com>
2023-09-09 11:57:54 +0530

    create column, image, and description

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Sathish liiyanga")
+    content = """ I am a 28-year-old Army soldier, dedicated to serving my country with honor and integrity.
+    My journey in the military has been filled with challenges that have shaped me into a disciplined and resilient individual. I am committed to the values of duty,
+    loyalty, and selfless service, which guide my actions both on and off the battlefield.
+In my time in the Army, I've learned the importance of teamwork and camaraderie.
+Whether it's during intense training exercises or in the midst of a mission, I can always rely on my fellow soldiers.
+We share a bond that goes beyond friendship; it's a brotherhood forged in the crucible of adversity.
+My dedication to physical fitness is unwavering.
+    """
+    st.info(content)
