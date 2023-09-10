2fa91eab8de14b1ad446d6865b78c757d002120d
Sathish Liyanage <sathishliyanage@gmail.com>
2023-09-10 13:58:08 +0530

    add links, coloumn space

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
@@ -18,3 +19,24 @@ We share a bond that goes beyond friendship; it's a brotherhood forged in the cr
 My dedication to physical fitness is unwavering.
     """
     st.info(content)
+    
+content2 = """ Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write('[Source code](https://github.com)')
+                
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write('[Source code](https://github.com)')
